43f56214e901c4a1cc5ba3c762d2851a992e2093
Dan <danhboi23@hotmail.com>
2020-03-15 14:24:12 -0700

    Updates

---
+++ precache-manifest.cb4d29a3b79976a8c91b32362e40deb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9d32674008a667153fb394d7d020ba",
+    "url": "/munsters/index.html"
+  },
+  {
+    "revision": "bc581ae0a3b73da92bcf",
+    "url": "/munsters/static/css/main.c9bc734b.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/munsters/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/munsters/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc581ae0a3b73da92bcf",
+    "url": "/munsters/static/js/main.7f620850.chunk.js"
+  },
+  {
+    "revision": "18064c1606e6f4efd94a",
+    "url": "/munsters/static/js/runtime-main.81d82310.js"
+  }
+]);
